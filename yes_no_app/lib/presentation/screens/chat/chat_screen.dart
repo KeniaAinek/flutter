6280524bd06df4cd658fa1cb19cee139eeffc3ec
k.castro <k.castro@mefasa.org>
2025-02-18 16:58:32 -0700

    Cambios en yes_no_app appbar y  widget de mensajes creados

---
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/other_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            // backgroundImage: NetworkImage(url),
+          ),
+        ),
+        title: Text('Mi amor 💕'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10 ),
+        child: Column(
+          children: [
+            Expanded( child: ListView.builder(itemBuilder: (context, index) {
+              return (index % 2 == 0 )
+                ? const OtherMessageBubble()
+                : const MyMessageBubble();
+             })),
+          
+            Text('Mundo'),
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
