7f76ac640d79fe647a130165b2f5f104892f2f76
k.castro <k.castro@mefasa.org>
2025-03-10 09:28:25 -0700

    Progress indicator finished

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -29,4 +29,11 @@ class MenuItem {
       link: '/cards',
       icon: Icons.credit_card
     ),
+
+    MenuItem(
+      title: 'ProgressIndicators', 
+      subTitle: 'Generales y controlados', 
+      link: '/progress',
+      icon: Icons.refresh_rounded
+      ),
   ];
