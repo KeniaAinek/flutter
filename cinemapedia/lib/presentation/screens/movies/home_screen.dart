11107bc830812cb1ccb6da61803dc45be523eaae
Kenia Castro <keniad.ctorres@gmail.com>
2025-03-17 21:52:52 -0700

    Slider horizontal teminado , Slider vertical en progreso

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,6 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
@@ -35,17 +37,22 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
 
     final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final moviesSlideShow = ref.watch( moviesSlideshowProvider );
 
-    if( nowPlayingMovies.length == 0 ) return CircularProgressIndicator();
+    return Column(
+      children: [
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index){
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text( movie.title),
-        );
-      }
+        CustomAppbar(),
+
+        MoviesSlideshow( movies: moviesSlideShow ),
+
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTile: 'Lunes 20',
+        ),
+      
+      ],
     );
   }
 }
