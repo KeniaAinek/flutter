34e83787e7537eaed08d40126883631cac2385d5
k.castro <k.castro@mefasa.org>
2025-04-21 16:59:06 -0700

    formulario con cubit empezado

--- forms_app/lib/presentation/screens/register_screen.dart
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -29,6 +29,8 @@ class _RegisterView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
 
+              SizedBox(height: 10),
+
               const FlutterLogo( size: 100),
 
               _RegisterForm(),
@@ -42,23 +44,72 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
 
-          CustomTextFormField(),
+          CustomTextFormField(
+            label: 'Nombre de usuario',
+            onChanged: (value) => username = value,
+            validator: (value){
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              if( value.length < 6 ) return 'Mas de 6 letras';
+              return null;
+            },
+          ),
           SizedBox(height: 10),
-          CustomTextFormField(),
+
+          CustomTextFormField(
+            label: 'Correo Electronico',
+            onChanged: (value) => email = value,
+            validator: (value){
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',);
+              if( !emailRegExp.hasMatch(value)) return 'No tiene formato de correo';
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+
+          CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value){
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              if( value.length < 6 ) return 'Mas de 6 letras';
+              return null;
+            },
+          ),
 
           const SizedBox(height: 20),
 
            FilledButton.tonalIcon(
-                onPressed: () => {}, 
+                onPressed: (){
+                  final isValid = _formKey.currentState!.validate();
+                  if(!isValid) return;
+                  print('$username, $email, $password');
+                }, 
                 icon: const Icon( Icons.save ),
                 label: const Text('Crear usuario'),
           ),
--- forms_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ forms_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final bool obscureText;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomTextFormField({
+    super.key, 
+    this.label, 
+    this.hint, 
+    this.errorMessage, 
+    this.obscureText = false,
+    this.onChanged, 
+    this.validator, 
+  });
 
   @override
   Widget build(BuildContext context) {
 
-    return TextFormField(
-      onChanged: (value){
-        print('value: $value');
-      },
-      validator: (value) {
-        if( value == null) return 'Campo ess requerido';
-        if( value.isEmpty ) return 'Campo ess requerido';
-        if( value.trim().isEmpty ) return 'Campo ess requerido';
+    final colors = Theme.of(context).colorScheme;
 
-        return null;
-      },
+    final border = OutlineInputBorder(
+      // borderSide: BorderSide(color: colors.primary),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
       decoration: InputDecoration(
-        
+        enabledBorder: border,
+        focusedBorder: border.copyWith(borderSide: BorderSide( color: colors.primary )),
+        errorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade800 )),
+        focusedErrorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade800 )),
+
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: colors.primary,
+        prefixIcon: Icon( Icons.supervised_user_circle_outlined, color: colors.primary),
       ),
     );
   }
